e860756efa62f5704e0e5871f84743c2cb5b0a03
Matt Gahrns <matthewgahrns@hotmail.com>
2019-10-22 10:13:00 -0700

    Added relationships.

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,3 +1,4 @@
 class Favorite < ActiveRecord::Base
-
+    belongs_to :movies
+    belongs_to :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
-
+    has_many :favorites
+    has_many :movies, through: :favorites
 end
