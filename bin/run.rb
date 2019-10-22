21d5e0aa0b08ffccd1d7830994148781dbe063ba
Matt Gahrns <matthewgahrns@hotmail.com>
2019-10-22 11:45:38 -0700

    Added find_movie_by_title method and new_movie helper method.

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,6 @@
 require_relative '../config/environment'
-
+require 'pry'
+require 'httparty'
 
 currUser = nil
 
@@ -15,4 +16,48 @@ def find_or_create_user_by_username
     end
 end
 
-#puts "HELLO WORLD"
+def find_movie_by_title
+    puts "Enter the title of the movie you wish to find:"
+    title = gets.chomp
+    url = "http://www.omdbapi.com/?t=#{title}&apikey=5b27a2ad"
+    response = HTTParty.get(url)
+    res = response.parsed_response
+    movie = new_movie(res)
+    movie
+end
+
+def new_movie(res)
+    title = res["Title"]
+    year = res["Year"]
+    rated = res["Rated"]
+    released = res["Released"]
+    runtime = res["Runtime"]
+    genre = res["Genre"]
+    director = res["Director"]
+    writer = res["Writer"]
+    actors = res["Actors"]
+    plot = res["Plot"]
+    language = res["Language"]
+    country = res["Country"]
+    awards = res["Awards"]
+    poster = res["Poster"]
+    metascore = res["Metascore"]
+    imdbrating = res["imdbRating"]
+    imdbvotes = res["imdbVotes"]
+    imdbid = res["imdbID"]
+    type = res["Type"]
+    dvd = res["DVD"]
+    boxoffice = res["BoxOffice"]
+    production = res["Production"]
+    website = res["Website"]
+
+    Movie.new(title: title, year: year, rated: rated, released: released, runtime: runtime, genre: genre, director: director, writer: writer, actors: actors, plot: plot, language: language, country: country, awards: awards, poster: poster, metascore: metascore, imdbrating: imdbrating, imdbvotes: imdbvotes, imdbid: imdbid, type: type, dvd: dvd, boxoffice: boxoffice, production: production, website: website)
+end
+
+
+
+
+
+puts "HELLO WORLD"
+Pry.start
+0
