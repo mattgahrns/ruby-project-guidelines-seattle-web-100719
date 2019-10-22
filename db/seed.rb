80a81e2b25656413b7a8dd82f7ba10f9347a2ffa
Matt Gahrns <matthewgahrns@hotmail.com>
2019-10-22 10:51:07 -0700

    Further filled out skeleton of seed.rb

--- db/seed.rb
+++ db/seed.rb
@@ -12,27 +12,27 @@ end
 responses.each do |response|
     title = response["Title"]
     year = response["Year"]
-    rated = 
-    released = 
-    runtime =
-    genre =
-    director =
-    writer =
-    actors =
-    plot =
-    language =
-    country =
-    awards =
-    poster =
-    metascore =
-    imdbrating =
-    imdbvotes =
-    imdbid =
-    type =
-    dvd =
-    boxoffice =
-    production =
-    website =
+    rated = response["Rated"]
+    released = response["Released"]
+    runtime = response["Runtime"]
+    genre = response["Genre"]
+    director = response["Director"]
+    writer = response["Writer"]
+    actors = response["Actors"]
+    plot = response["Plot"]
+    language = response["Language"]
+    country = response["Country"]
+    awards = response["Awards"]
+    poster = response["Poster"]
+    metascore = response["Metascore"]
+    imdbrating = response["imdbRating"]
+    imdbvotes = response["imdbVotes"]
+    imdbid = response["imdbID"]
+    type = response["Type"]
+    dvd = response["DVD"]
+    boxoffice = response["BoxOffice"]
+    production = response["Production"]
+    website = response["Website"]
 
 
 
