85a535872e93e98a0756864ef0191f2e31bb654a
Matt Gahrns <matthewgahrns@hotmail.com>
2019-10-22 11:31:53 -0700

    Implemented find_or_create_user_by_username method.

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,18 @@
 require_relative '../config/environment'
 
 
+currUser = nil
 
-puts "HELLO WORLD"
+def find_or_create_user_by_username
+    puts "To start enter your username or create one and press enter to sign up:"
+    username = gets.chomp
+    User.all.each do |user|
+        if user.username == username
+            currUser = user
+        else
+            currUser = User.create(username: username)
+        end
+    end
+end
+
+#puts "HELLO WORLD"
