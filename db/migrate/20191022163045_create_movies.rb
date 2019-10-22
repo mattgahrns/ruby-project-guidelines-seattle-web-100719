a6f50fb9da14073b45911ccfd6709a821c206ce8
Matt Gahrns <matthewgahrns@hotmail.com>
2019-10-22 09:57:25 -0700

    Created rake migrate files for movies, users, and favorites.

---
+++ db/migrate/20191022163045_create_movies.rb
@@ -0,0 +1,28 @@
+class CreateMovies < ActiveRecord::Migration[5.0]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :year
+      t.string :released
+      t.string :runtime
+      t.string :genre
+      t.string :director
+      t.string :writer
+      t.string :actors
+      t.string :plot
+      t.string :language
+      t.string :country
+      t.string :awards
+      t.string :poster
+      t.string :metascore
+      t.string :imdbrating
+      t.string :imdbvotes
+      t.string :imdbid
+      t.string :type
+      t.string :dvd
+      t.string :boxoffice
+      t.string :production
+      t.string :website
+    end
+  end
+end
